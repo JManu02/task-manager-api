ff7d5af6424f220ee1e82f4d2ad6494b717311b4
JManu02 <106283478+JManu02@users.noreply.github.com>
2026-04-07 16:51:51 -0600

    Commit 7/4/2026
    Finalización de la tercera fase del proyecto que abarco los auth, role y middlewares

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -6,10 +6,22 @@ const {
   refreshController,
   logoutController
 } = require('../controllers/auth.controller')
+const verifyToken = require('../middlewares/verifyToken')
+const requireRole = require('../middlewares/requireRole')
 
 router.post('/register', registerController)
 router.post('/login', loginController)
 router.post('/refresh', refreshController)
 router.post('/logout', logoutController)
+
+// Ruta de prueba
+router.get('/me', verifyToken, (req, res) => {
+  res.json({ message: 'Access granted', user: req.user })
+})
+
+// Ruta de prueba solo admin
+router.get('/admin', verifyToken, requireRole('admin'), (req, res) => {
+  res.json({ message: 'Welcome admin' })
+})
 
 module.exports = router
